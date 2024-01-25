84b6db1a6282975e3f8f54dc61a8515adba9b938
yixx759 <126923383+yixx759@users.noreply.github.com>
2024-01-25 23:13:15 +0000

    Add files via upload

--- Volumetric.cs
+++ Volumetric.cs
@@ -19,11 +19,21 @@ public class Volumetric : MonoBehaviour
      public static DownSample Downsamp;
     [SerializeField ,Range(-1,1)] private float _Albedo;
     [SerializeField ] private float _Phi;
-    [SerializeField ] private int _samples =20 ;
+    [SerializeField, Range(1,1000) ] private int _samples =20 ;
     [SerializeField ] private Color _Fcol = Vector4.one ;
+  
 
+    
+    
+    
+    //redefine vert for last
+    //show alternative done by unity
+    
+    
+    
     private void OnEnable()
     {
+        print(VolShader.passCount);
         Downsamp = assighn;
     }
 
@@ -41,29 +51,48 @@ public class Volumetric : MonoBehaviour
         VolShader.SetMatrix("InverseProj", projectionMatrix.inverse);
 
         RenderTexture resolu;
+        RenderTexture resolud;
         if (Downsamp == DownSample.Full)
         {
              resolu = RenderTexture.GetTemporary(Screen.width, Screen.height);
+             resolud = RenderTexture.GetTemporary(Screen.width, Screen.height);
             
         }
         else if (Downsamp == DownSample.Half)
         {
             resolu = RenderTexture.GetTemporary(Screen.width/2, Screen.height/2);
-            
+            resolud = RenderTexture.GetTemporary(Screen.width/2, Screen.height/2);
+        
         }
         else
         {
             resolu = RenderTexture.GetTemporary(Screen.width/4, Screen.height/4);
+           resolud = RenderTexture.GetTemporary(Screen.width/4, Screen.height/4);
+          
         }
+       
 
 
-
-        Graphics.Blit(source,resolu,VolShader,0);
         
-        Graphics.Blit(resolu,destination);
-        
-       Graphics.Blit(source,destination,VolShader,1);
+       Graphics.Blit(source,resolu,VolShader,0);
+      VolShader.SetTexture("Quarter" ,resolu );
+       
+     Graphics.Blit(source,resolud, VolShader,3);
+     VolShader.SetTexture("QuarterD" ,resolud );
+    
+     Graphics.Blit(source,destination);
+      
+     Graphics.Blit(source,destination, VolShader, 4);
+           
+           
+     
+     
+      
+
+      
+    
        resolu.Release();
+       resolud.Release();
         
     }
 }
